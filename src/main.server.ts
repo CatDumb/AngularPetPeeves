ae69bc21a41f85a60f7c2268e4983cf8eb1178ad
CatDumb <khoa.nguyenminh2@nashtechglobal.com>
2025-09-16 14:22:36 +0700

    fix: NG401 templating

--- src/main.server.ts
+++ src/main.server.ts
@@ -1,7 +1,20 @@
-import { bootstrapApplication } from '@angular/platform-browser';
+import { bootstrapApplication, BootstrapContext } from '@angular/platform-browser';
 import { App } from './app/app';
-import { config } from './app/app.config.server';
+import { appConfig } from './app/app.config';
+import { config as serverConfig } from './app/app.config.server';
 
-const bootstrap = () => bootstrapApplication(App, config);
+export const bootstrap = (context: BootstrapContext) =>
+  bootstrapApplication(
+    App,
+    {
+      ...(appConfig ?? {}),
+      ...(serverConfig ?? {}),
+      providers: [
+        ...((appConfig?.providers as any[]) ?? []),
+        ...((serverConfig?.providers as any[]) ?? []),
+      ],
+    },
+    context
+  );
 
 export default bootstrap;
